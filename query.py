f144050be8f9f05b57765e749ce9a365750dbee3
Jain <rjain11@BANL15371b7df.local>
2017-06-16 09:59:40 +0530

    script for fetching data from database

---
+++ query.py
@@ -0,0 +1,30 @@
+import MySQLdb 
+import datetime
+
+HOST = "localhost"
+USER = "root"
+PASSWD = "roopal"
+DATABASE = "hackathon"
+
+def fetch_query(tweet_id):
+    db = MySQLdb.connect(host=HOST, user=USER, passwd=PASSWD, db=DATABASE, charset="utf8")
+    cursor = db.cursor()
+    query = "SELECT * from tweet_info where tweet_id = {}; ".format(tweet_id)
+    cursor.execute(query)
+    data_main = cursor.fetchone()
+    query = "SELECT * from tweet_info where parent_id = {}; ".format(tweet_id)
+    cursor.execute(query)
+
+    data = cursor.fetchall()
+    data = list(data)
+    data.append(data_main)
+   
+
+    cursor.close()
+    db.close()
+
+    return data
+
+if __name__ == '__main__':
+    data = fetch_query(875558348506189824)
+    print (data)
